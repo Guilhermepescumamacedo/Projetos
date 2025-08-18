8542830ec1ea1bdfbfca9834981b7e9f3c1674de
GUILHERME PESCUMA <gui.pescumamacedo@gmail.com>
2025-08-18 17:12:36 -0300

    Update Atividade 3.py
    Ele define uma função que soma todos os números de uma lista e retorna o resultado. Em seguida, chama essa função com uma lista pequena para mostrar como funciona.

--- Projetos/Semana 3/Atividade 3.py
+++ Projetos/Semana 3/Atividade 3.py
@@ -4,7 +4,7 @@ def soma_lista(lista):
     for num in lista:
         total += num  # Acumula cada valor na variável 'total'
     return total  # Retorna a soma total
-
+ 
 # Chamada da função com a lista [1, 2]
 lista = [1, 2]
 print(soma_lista(lista))  # Saída será 3
